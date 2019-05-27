a729abea6f25e358181af24e7eca4ede8f1b0d29
linux <linux@debian>
2019-05-27 12:32:15 +0200

    outputs the number of properties& check if a value exists

--- student.js
+++ student.js
@@ -24,3 +24,23 @@ for (var std in student){
 
     console.log(student[std])
 }
+
+//outputs the number of properties
+var stdLength = Object.keys(student);
+console.log(stdLength.length);
+
+
+
+//fuction checks if a certain value does exist
+
+function hasProperty(std){
+if(student.hasOwnProperty(std)){
+return true;
+}
+else return false;
+
+
+
+}
+
+console.log(hasProperty("class"));
